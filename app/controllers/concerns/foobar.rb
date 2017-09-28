bc514167374f39697ef95d602b157229de24ef33
Alice Deng <alicedeng@airbears2-10-142-160-74.airbears2.1918.berkeley.edu>
2017-09-27 17:58:25 -0700

    lol

---
+++ app/controllers/concerns/foobar.rb
@@ -0,0 +1,10 @@
+class Foobar
+  # ENTER CODE FOR Q2 HERE
+  def initialize(baz)
+  	@bee = baz 
+  end
+
+  def bar(lol, sat:, dat:)
+  	return lol.to_s + @bee + sat.to_s
+  end
+end
---
+++ app/controllers/concerns/person.rb
@@ -0,0 +1,25 @@
+class Person
+  attr_accessor :name, :age
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+
+  end
+
+  def nickname
+    # YOUR IMPLEMENTATION HERE
+    return @name[0...4]
+  end
+
+  def birth_year
+    # YOUR IMPLEMENTATION HERE
+    return Time.now.year.to_i - @age.to_i
+  end
+
+  def introduction
+    # YOUR IMPLEMENTATION HERE
+    return @name + @age.to_s
+  end
+
+end
